18878d33fbaf109abef4182b64f3a815ed856fe7
DuyBua <mondorea42@gmail.com>
2025-08-06 22:01:50 +0700

    feat: Develop import student from excel file and add models

---
+++ uef_diem_danh/Models/DiemDanh.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace uef_diem_danh.Models
+{
+    [Table("DiemDanh")]
+    public class DiemDanh
+    {
+        [Key]
+        public int MaDiemDanh { get; set; }
+
+        public DateTime ThoiGianDiemDanh { get; set; }
+
+        public bool TrangThai { get; set;}
+
+
+        // relationships
+        public int MaHocVien {  get; set; }
+        public HocVien HocVien { get; set; }
+
+        public int MaBuoiHoc {  get; set; }
+        public BuoiHoc BuoiHoc { get; set; }
+        
+    }
+}
---
+++ uef_diem_danh/Models/LopHoc.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace uef_diem_danh.Models
+{
+    [Table("LopHoc")]
+    public class LopHoc
+    {
+        [Key]
+        public int MaLopHoc { get; set; }
+
+        public string TenLopHoc { get; set;}
+
+        public DateOnly ThoiGianBatDau {  get; set;}
+
+        public DateOnly ThoiGianKetThuc {  get; set;}
+
+        public DateTime CreatedAt { get; set;} = DateTime.UtcNow;
+
+        // relationships
+        public ICollection<ThamGia> ThamGias { get; set; }
+        public ICollection<BuoiHoc> BuoiHocs { get; set; }
+
+    }
+}
