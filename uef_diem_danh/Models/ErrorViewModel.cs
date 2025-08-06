55a6efa079a0430ea31f962414d7b145c7059b11
anhle19 <leanh0094@gmail.com>
2025-08-06 13:04:39 +0700

    first commit

---
+++ uef_diem_danh/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace uef_diem_danh.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
