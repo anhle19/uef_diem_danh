cdf62455bd0a041ffd8723046c6d3ddbdeeb84d3
DuyBua <mondorea42@gmail.com>
2025-08-07 11:20:39 +0700

    feat: Develop get latest list of studying class for management

---
+++ uef_diem_danh/DTOs/StudyClassListManagementDto.cs
@@ -0,0 +1,15 @@
+﻿namespace uef_diem_danh.DTOs
+{
+    public class StudyClassListManagementDto
+    {
+        public int Id { get; set; }
+
+        public string StudyClassName { get; set; }
+
+        public DateOnly StartDate { get; set; }
+
+        public DateOnly EndDate { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
