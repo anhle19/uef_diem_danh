5e24e05c0f591993d5d2e08d72ab3efb96049ba4
DuyBua <mondorea42@gmail.com>
2025-08-11 21:42:25 +0700

    feat: Add foreign key attribute to MaLopHoc column in BuoiHoc table

---
+++ uef_diem_danh/Migrations/20250811143148_FixMaLopHocColumnInBuoiHocTable.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace uef_diem_danh.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixMaLopHocColumnInBuoiHocTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BuoiHoc_LopHoc_LopHocMaLopHoc",
+                table: "BuoiHoc");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BuoiHoc_LopHocMaLopHoc",
+                table: "BuoiHoc");
+
+            migrationBuilder.DropColumn(
+                name: "LopHocMaLopHoc",
+                table: "BuoiHoc");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BuoiHoc_MaLopHoc",
+                table: "BuoiHoc",
+                column: "MaLopHoc");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BuoiHoc_LopHoc_MaLopHoc",
+                table: "BuoiHoc",
+                column: "MaLopHoc",
+                principalTable: "LopHoc",
+                principalColumn: "MaLopHoc",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BuoiHoc_LopHoc_MaLopHoc",
+                table: "BuoiHoc");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BuoiHoc_MaLopHoc",
+                table: "BuoiHoc");
+
+            migrationBuilder.AddColumn<int>(
+                name: "LopHocMaLopHoc",
+                table: "BuoiHoc",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BuoiHoc_LopHocMaLopHoc",
+                table: "BuoiHoc",
+                column: "LopHocMaLopHoc");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BuoiHoc_LopHoc_LopHocMaLopHoc",
+                table: "BuoiHoc",
+                column: "LopHocMaLopHoc",
+                principalTable: "LopHoc",
+                principalColumn: "MaLopHoc",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
