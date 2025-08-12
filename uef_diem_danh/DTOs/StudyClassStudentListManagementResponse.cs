0cb08fbbb84a5be3faa09ed709457e226c21d16a
DuyBua <mondorea42@gmail.com>
2025-08-12 16:17:53 +0700

    feat: Remove email and bar code column in student in study class management

--- uef_diem_danh/DTOs/StudyClassStudentListManagementResponse.cs
+++ uef_diem_danh/DTOs/StudyClassStudentListManagementResponse.cs
@@ -8,13 +8,9 @@
 
         public string FirstName { get; set; }
 
-        public string BarCode { get; set; }
-
         public DateOnly DateOfBirth { get; set; }
 
         public string Address { get; set; }
-
-        public string Email { get; set; }
 
         public string PhoneNumber { get; set; }
 
