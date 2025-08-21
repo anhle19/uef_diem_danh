a3db796551c7137db6a80722a54a73932d172008
DuyBua <mondorea42@gmail.com>
2025-08-21 13:29:12 +0700

    feat: Add DonVi column and change HinhColumn to nullable in HocVien Table

---
+++ uef_diem_danh/Migrations/20250821062818_AddDonViColumnAndChangeHinhAnhToNullableInHocVienTable.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace uef_diem_danh.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDonViColumnAndChangeHinhAnhToNullableInHocVienTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "HinhAnh",
+                table: "HocVien",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DonVi",
+                table: "HocVien",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DonVi",
+                table: "HocVien");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "HinhAnh",
+                table: "HocVien",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
