2a4e3e772a6342b2545902f7d06eb097522e5a82
DuyBua <mondorea42@gmail.com>
2025-08-22 16:10:11 +0700

    feat: Add events table

---
+++ uef_diem_danh/Models/SuKien.cs
@@ -0,0 +1,19 @@
+﻿namespace uef_diem_danh.Models
+{
+    public class SuKien
+    {
+        public int Id { get; set; }
+
+        public string TieuDe { get; set; }
+
+        public string NguoiPhuTrach { get; set; }
+
+        public int SoLuongDuKien { get; set; }
+
+        public bool TrangThai { get; set; }
+
+        public DateTime ThoiGian { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
