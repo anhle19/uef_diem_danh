4b1b8b9d7fe7a5f18c03dfa28bd51c4cb630b584
DuyBua <mondorea42@gmail.com>
2025-08-23 08:09:59 +0700

    feat: Add Diem danh su kien table

---
+++ uef_diem_danh/Migrations/20250823010544_CreateDiemDanhSuKienTable.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace uef_diem_danh.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateDiemDanhSuKienTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DiemDanhSuKiens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DonVi = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Ho = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Ten = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SoDienThoai = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NgaySinh = table.Column<DateOnly>(type: "date", nullable: true),
+                    AttendanceDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EventId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DiemDanhSuKiens", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DiemDanhSuKiens_SuKiens_EventId",
+                        column: x => x.EventId,
+                        principalTable: "SuKiens",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DiemDanhSuKiens_EventId",
+                table: "DiemDanhSuKiens",
+                column: "EventId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DiemDanhSuKiens");
+        }
+    }
+}
