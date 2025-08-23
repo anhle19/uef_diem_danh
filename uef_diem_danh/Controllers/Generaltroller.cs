18933447b9c08ba22e0dcafcfa060746da923d36
DuyBua <mondorea42@gmail.com>
2025-08-23 09:26:20 +0700

    feat: Add attendance event page

--- uef_diem_danh/Controllers/Generaltroller.cs
+++ uef_diem_danh/Controllers/Generaltroller.cs
@@ -1,20 +1,39 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using uef_diem_danh.Database;
+using uef_diem_danh.DTOs;
 
 namespace uef_diem_danh.Controllers
 {
     public class Generaltroller : Controller
     {
 
+        private readonly AppDbContext context;
+
+        public Generaltroller(AppDbContext context)
+        {
+            this.context = context;
+        }
 
 
         [Authorize(Roles = "Admin,Staff")]
         [Route("")]
         [HttpGet]
-        public IActionResult GetMainDashboardPage()
+        public async Task<IActionResult> GetMainDashboardPage()
         {
 
-            return View("~/Views/Dashboard.cshtml");
+            string teahcerRoleId = context.Roles.FirstOrDefault(r => r.Name == "Staff").Id;
+
+            DashboardResponse dashboardResponse = new DashboardResponse
+            {
+                NumberOfTeachers = context.UserRoles.Count(ur => ur.RoleId == teahcerRoleId),
+                NumberOfStudents = context.HocViens.Count(),
+                NumberOfStudyClass = context.LopHocs.Count(),
+                NumberOfClassSessions = context.BuoiHocs.Count()
+            };
+
+            return View("~/Views/Dashboard.cshtml", dashboardResponse);
         }
 
 
