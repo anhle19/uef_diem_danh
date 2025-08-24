8dafca455ca31ebf66cef52e02a224b3cb21f744
DuyBua <mondorea42@gmail.com>
2025-08-24 21:51:36 +0700

    feat: Add event attendance result page

--- uef_diem_danh/Controllers/EventController.cs
+++ uef_diem_danh/Controllers/EventController.cs
@@ -53,6 +53,37 @@ namespace uef_diem_danh.Controllers
             return View("~/Views/Events/AttendanceEvent.cshtml", classEvent);
         }
 
+        [Route("ket-qua-diem-danh-su-kien/{event_id}")]
+        public async Task<IActionResult> GetAttendanceEventResultPage(int event_id)
+        {
+
+            List<EventAttendanceResultParticipant> presentParticipants = await context.DiemDanhSuKiens
+                .Where(ddsk => ddsk.EventId == event_id)
+                .Select(ddsk => new EventAttendanceResultParticipant
+                {
+                    ParticipantFirstName = ddsk.Ten,
+                    ParticipantLastName = ddsk.Ho,
+                    StudyCenter = ddsk.DonVi,
+                    AttendanceDayTime = ddsk.AttendanceDate
+                })
+                .ToListAsync();
+
+            EventAttendanceResultResponse attendanceResult = await context.SuKiens
+                .Where(sk => sk.Id == event_id)
+                .Select(sk => new EventAttendanceResultResponse
+                {
+                    EventId = event_id,
+                    EventTitle = sk.TieuDe,
+                    ExpectedNumberOfParticipants = sk.SoLuongDuKien,
+                    PresentParticipants = presentParticipants
+                })
+                .FirstOrDefaultAsync();
+
+            ViewBag.EventId = event_id;
+
+            return View("~/Views/Events/ResultView.cshtml", attendanceResult);
+        }
+
         [Authorize(Roles = "Admin")]
         [Route("tao-su-kien")]
         [HttpPost]
